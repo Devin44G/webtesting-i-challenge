8fa87a06543e9d9879fd92b7ba0498c2bdfa9f25
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-03-26 13:59:40 -0500

    Fail function finished -> Fail tests passing -> MVP met

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -26,7 +26,7 @@ describe('Enhancer', () => {
       expect(EnhancedArmor.enhancement).toBe(11);
     });
     it('should return the object with the enhancment unchanged', () => {
-      newItem = Item;
+      let newItem = Item;
       newItem.enhancement = 20;
 
       let UnenhancedArmor = enhancer.succeed(newItem);
@@ -35,5 +35,38 @@ describe('Enhancer', () => {
       expect(UnenhancedArmor.enhancement).toBe(20);
     });
   });
+// FAIL TESTS
+  describe('fail() method', () => {
+    it('should decrease item durabiltiy by 5 if enhancement is less than 15', () => {
+      let newItem = Item;
+      newItem.enhancement = 14;
 
+      let FailedArmor = enhancer.fail(newItem);
+
+      expect(FailedArmor.durability).toBe(60);
+      console.log(newItem);
+    });
+    it('should decrease item durability by 10 if enhancement is greater than or equal to 15', () => {
+      let newItem = Item;
+      newItem.enhancement = 15;
+
+      let FailedArmor = enhancer.fail(newItem);
+
+      //expect(FailedArmor.durability).toBe(65); // <-- Expect to fail
+      expect(FailedArmor.durability).toBe(55);
+      console.log(newItem);
+    });
+    it('should decrease item enhancement by 1 if enhancement is greater than 16', () => {
+      let newItem = Item;
+      newItem.enhancement = 18;
+
+      let FailedArmor = enhancer.fail(newItem);
+
+      //expect(FailedArmor.durability).toBe(65); // <-- Expect to fail
+      //expect(FailedArmor.durability).toBe(18); // <-- Expect to fail
+      expect(FailedArmor.enhancement).toBe(17);
+      expect(FailedArmor.durability).toBe(55);
+      console.log(FailedArmor);
+    });
+  });
 });
