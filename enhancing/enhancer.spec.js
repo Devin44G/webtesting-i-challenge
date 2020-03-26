feb49149a034dc0b564997e9b4e1d133e8e997da
Devin44G <35251347+Devin44G@users.noreply.github.com>
2020-03-26 12:24:26 -0500

    Repair function finished -> repair test passing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,20 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+const Item = {
+  name: 'Bronze Helm',
+  durability: 65,
+  enhancement: 20
+};
+
+// REPAIR TESTS
+describe('Enhancer', function() {
+  describe('repair() method', function() {
+    it('Should take in an object to repair, and output an object with a durability of 100', function() {
+      let ArmorPiece = enhancer.repair(Item);
+
+      //expect(ArmorPiece.durability).toBe(65); // <-- Expect to fail
+      expect(ArmorPiece.durability).toBe(100);
+    });
+  });
+});
